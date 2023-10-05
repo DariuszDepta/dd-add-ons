8bbe0849766e36f27832576a384792ba4dd85651
Dariusz Depta <dariusz@confio.gmbh>
2023-10-05 18:30:37 +0200

    Updates.

--- cw-multi-test/dd-tarpaulage.sh
+++ cw-multi-test/dd-tarpaulage.sh
@@ -7,4 +7,12 @@
 #
 ###############################################################################
 
-cargo tarpaulin --workspace --features backtrace,cosmwasm_1_1,cosmwasm_1_2,cosmwasm_1_3,iterator,staking,stargate --force-clean --out Html --engine llvm --output-dir ./target/tov
+WORKING_DIRECTORY=$(pwd)
+
+cargo tarpaulin --workspace --features backtrace,cosmwasm_1_1,cosmwasm_1_2,cosmwasm_1_3,iterator,staking,stargate --force-clean --out Html --engine llvm --output-dir "$WORKING_DIRECTORY/target/tov"
+
+# display final message
+echo ""
+echo "Open coverage report:"
+echo "  HTML file://$WORKING_DIRECTORY/target/tov/tarpaulin-report.html"
+echo ""
